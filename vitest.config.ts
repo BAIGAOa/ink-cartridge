403570f74499c3e2eb885e61187c821318d7e679
BAIGAO <3416238595@qq.com>
2026-05-24 14:48:36 +0800

    fix: You should not use jsdom to test ink and terminal environments

--- vitest.config.ts
+++ vitest.config.ts
@@ -2,8 +2,14 @@ import { defineConfig } from 'vitest/config';
 
 export default defineConfig({
   test: {
-    environment: 'jsdom',
-    include: ['src/__tests__/**/*.test.{ts,tsx}'],
+    environment: 'jsdom', // Due to legacy issues, jsdom is used incorrectly, but existing tests cannot be separated from it, so backward compatibility is used here.
+    include: ['src/__tests__/**/*.test.{ts,tsx}'], 
     globals: true,
+    environmentMatchGlobs: [
+      // 为 .ink.test.ts 和 .ink.test.tsx 文件使用 node 环境 
+      // These suffixes all mean that they automatically use the node environment.
+      ['src/__tests__/**/*.ink.test.ts', 'node'],
+      ['src/__tests__/**/*.ink.test.tsx', 'node'],
+    ],
   },
 });
